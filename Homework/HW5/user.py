e20abfd26218b2dcfc85db10f5716c407c6e4940
Jiyoung <thdwldud8@gmail.com>
2022-04-04 20:17:55 +0900

    HW5 코드 수정

--- Homework/HW5/user.py
+++ Homework/HW5/user.py
@@ -1,19 +1,16 @@
 from socket import *
 import time
+
 BUF_SIZE = 1024
 
+
 while True:
-    user = input()
+    user = input('Enter the device number 1 or 2: ')
     
-    # 디바이스 1
-    device1 = socket(AF_INET, SOCK_STREAM)
-    device1.connect(('localhost', 7777))
-
-    # 디바이스 2
-    device2 = socket(AF_INET, SOCK_STREAM)
-    device2.connect(('localhost', 8888))
-
     if user == '1':
+        # 디바이스 1 연결
+        device1 = socket(AF_INET, SOCK_STREAM)
+        device1.connect(('localhost', 7777))
         device1.send(b'Request')
         msg = device1.recv(BUF_SIZE)
         data = msg.decode()
@@ -24,8 +21,12 @@ while True:
         f = open('data.txt', 'a')
         f.write(req)
         f.close()
+        device1.close()
 
     elif user == '2':
+        # 디바이스 2 연결
+        device2 = socket(AF_INET, SOCK_STREAM)
+        device2.connect(('localhost', 8888))
         device2.send(b'Request')
         msg = device2.recv(BUF_SIZE)
         data = msg.decode()
@@ -36,12 +37,20 @@ while True:
         f = open('data.txt', 'a')
         f.write(req)
         f.close()
+        device2.close()
 
     elif user == 'quit':
+        # 디바이스 1 연결
+        device1 = socket(AF_INET, SOCK_STREAM)
+        device1.connect(('localhost', 7777))
+        # 디바이스 2 연결
+        device2 = socket(AF_INET, SOCK_STREAM)
+        device2.connect(('localhost', 8888))
+        
         device1.send(b'quit')
+        device1.close()
+        
         device2.send(b'quit')
+        device2.close()
         break
-
-    device1.close()
-    device2.close()
 
